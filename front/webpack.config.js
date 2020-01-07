e77cdae723b4ffd9aaef6bcc94e0167e78d3134b
Masaya Akiyama <makiyama-tky@umin.ac.jp>
2020-01-07 19:39:21 +0900

    Create docker file

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
-  entry: ['./src/index.tsx'],
+  entry: [__dirname + '/src/index.tsx'],
   output: {
     filename: 'bundle.js',
     path: __dirname + '/dist'
